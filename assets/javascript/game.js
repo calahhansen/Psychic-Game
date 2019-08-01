6adea512c561f49a8d130512fb6766bbdc3d15e3
Calah Hansen <calah.hansen@gmail.com>
2019-08-01 15:05:06 -0500

    added function and onkey and defined text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,26 +4,40 @@ const compGuess = abc[Math.floor(Math.random() * abc.length) ];
 console.log(compGuess);
 console.log(abc.length);
 
-document.onkeyup = function(event) {
-    const userGuess = event.key;
-    const compGuess = abc[Math.floor(Math.random() * abc.length) ];
-    console.log(compGuess);
-    console.log(userGuess);
-}
-
-const wins = 0;
-const losses = 0;
-const clicks = 0;
+let wins = 0;
+let losses = 0;
+let clicks = 0;
+let guesses = 0;
 
 const winsText = document.getElementById("wins-text");
 const lossesText = document.getElementById("losses-text");
 const clicksText = document.getElementById("clicks-text");
 const userGuesses = document.getElementById("user-guesses")
 
+document.onkeyup = function(event) {
+    const userGuess = event.key;
+    console.log(userGuess);
+    if (clicks > 9) {
+        if (userGuess === compGuess) {
+        wins++;
+        winsText.textContent = "Wins: " + wins;
+        } else if (userGuess !== compGuess) {
+        clicks++;
+        clicksText.textContent = "Number of Guesses: " + clicks;
+        userGuesses.textContent = "You have guessed the following: " + userGuesses;
+        } else {
+            losses++;
+            lossesText.textContent = "Losses: " + losses;
+        }
+    }
+}
 
-winsText.textContent = ("Wins: " + wins);
-lossesText.textContent = "Losses: " + losses;
-clicksText.textContent = "Number of Guesses: " + clicks;
-userGuesses.textContent = "You have guessed the following: " + userGuess;
+
+
+
+
+
+
+
 
 
